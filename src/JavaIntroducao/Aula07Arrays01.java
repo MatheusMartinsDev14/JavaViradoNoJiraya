5f2ec1246c656f4fa18c055c4a9dc6b879343362
Matheus Martins <matheusmartinsdeveloper14@gmail.com>
2025-07-28 12:06:16 -0300

    Aula 07

---
+++ src/JavaIntroducao/Aula07Arrays01.java
@@ -0,0 +1,18 @@
+package JavaIntroducao;
+
+public class Aula07Arrays01 {
+    public static void main(String[] args) {
+
+      int [] idades = new int[3];
+      idades[0] = 20;
+      idades[1] = 18;
+      idades[2] = 21;
+      idades[3] = 22;
+
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+        System.out.println(idades[3]);
+
+    }
+}
