267dd3687e68c113613fc511376b655fca7270a0
Matheus Martins <matheusmartinsdeveloper14@gmail.com>
2025-07-29 09:50:07 -0300

    Aula 07

---
+++ src/JavaIntroducao/Aula07Arrays02.java
@@ -0,0 +1,15 @@
+package JavaIntroducao;
+
+public class Aula07Arrays02 {
+    public static void main(String[] args) {
+
+        String [] nomes = new String[3];
+        nomes[0] = "Goku";
+        nomes[1] = "Kurosaki";
+        nomes[2] = "Naruto";
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(nomes[i]);
+        }
+    }
+}
