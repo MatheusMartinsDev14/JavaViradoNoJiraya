2313678e87752ab81e1057b4faf6a4d31a7c078e
Matheus Martins <matheusmartinsdeveloper14@gmail.com>
2025-07-30 13:13:42 -0300

    Introdução a Programação Orientada a Objetos

---
+++ src/javacore/intruducaoclasses/dominio/Estudante.java
@@ -0,0 +1,7 @@
+package javacore.intruducaoclasses.dominio;
+
+public class Estudante {
+    public String nome;
+    public int idade;
+    public String sexo;
+}
---
+++ src/javacore/intruducaoclasses/test/EstudanteTest01.java
@@ -0,0 +1,10 @@
+package javacore.intruducaoclasses.test;
+
+import javacore.intruducaoclasses.dominio.Estudante;
+
+public class EstudanteTest01 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+
+    }
+}
