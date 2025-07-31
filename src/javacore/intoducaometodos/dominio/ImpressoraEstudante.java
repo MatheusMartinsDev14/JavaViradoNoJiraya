ddbe9188c7137012cceb1e5a763310eaff5afc7f
Matheus Martins <matheusmartinsdeveloper14@gmail.com>
2025-07-31 14:28:11 -0300

    Parâmetros tipo refêrencia

---
+++ src/javacore/intoducaometodos/dominio/ImpressoraEstudante.java
@@ -0,0 +1,15 @@
+package javacore.intoducaometodos.dominio;
+
+public class ImpressoraEstudante {
+    public void imprime(Estudante estudante){
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+    }
+
+    public void imprime02(Estudante estudante02){
+        System.out.println(estudante02.nome);
+        System.out.println(estudante02.idade);
+        System.out.println(estudante02.sexo);
+    }
+}
---
+++ src/javacore/intoducaometodos/test/EstudanteTest01.java
@@ -0,0 +1,37 @@
+package javacore.intoducaometodos.test;
+
+import javacore.intoducaometodos.dominio.Estudante;
+import javacore.intoducaometodos.dominio.ImpressoraEstudante;
+
+public class EstudanteTest01 {
+    public static void main(String[] args) {
+        Estudante estudante01 = new Estudante();
+        Estudante estudante02 = new Estudante();
+
+        ImpressoraEstudante impressora = new ImpressoraEstudante();
+
+        estudante01.nome = "Midoriya";
+        estudante01.idade = 15;
+        estudante01.sexo = 'M';
+
+        estudante02.nome = "Sakura";
+        estudante02.idade = 16;
+        estudante02.sexo = 'F';
+
+        impressora.imprime(estudante01);
+
+        System.out.println(estudante01.nome);
+        System.out.println(estudante01.idade);
+        System.out.println(estudante01.sexo);
+
+        System.out.println("----------------------------");
+
+        System.out.println(estudante02.nome);
+        System.out.println(estudante02.idade);
+        System.out.println(estudante02.sexo);
+
+        impressora.imprime(estudante02);
+
+    }
+
+}
