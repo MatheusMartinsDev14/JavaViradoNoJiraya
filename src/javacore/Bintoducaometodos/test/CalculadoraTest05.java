21ebfd6ee8c0768d8bea6f879f4235508085a7ce
Matheus Martins <matheusmartinsdeveloper14@gmail.com>
2025-08-10 17:21:07 -0300

    Aula 52

---
+++ src/javacore/Bintoducaometodos/test/CalculadoraTest05.java
@@ -0,0 +1,14 @@
+package javacore.Bintoducaometodos.test;
+
+import javacore.Bintoducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest05 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        int [] numeros = {1, 2, 3, 4, 5};
+        calculadora.somaArray(numeros);
+        calculadora.somaVarArgs(1,2,3,4,5);
+
+
+    }
+}
