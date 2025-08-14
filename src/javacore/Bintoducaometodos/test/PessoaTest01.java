10d31f33f9f9d97e4d6153e93b169c426c400c83
Matheus Martins <matheusmartinsdeveloper14@gmail.com>
2025-08-14 10:18:17 -0300

    Aula 54

---
+++ src/javacore/Bintoducaometodos/test/PessoaTest01.java
@@ -0,0 +1,12 @@
+package javacore.Bintoducaometodos.test;
+
+import javacore.Bintoducaometodos.dominio.Pessoa;
+
+public class PessoaTest01 {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Jiraya");
+        pessoa.setIdade(70);
+        pessoa.imprime();
+    }
+}
