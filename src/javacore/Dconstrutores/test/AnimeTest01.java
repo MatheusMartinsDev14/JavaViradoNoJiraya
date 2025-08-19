d06056130c63b8e5baa501f94c352c5449f93576
Matheus Martins <matheusmartinsdeveloper14@gmail.com>
2025-08-19 11:06:25 -0300

    Aula 59

--- src/javacore/Dconstrutores/test/AnimeTest01.java
+++ src/javacore/Dconstrutores/test/AnimeTest01.java
@@ -4,7 +4,7 @@ import javacore.Dconstrutores.dominio.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime("Naturo Shippuden", "TV", 700, "Ação");
+        Anime anime = new Anime("Naturo Shippuden", "TV", 700, "Ação", "Production IG");
         anime.imprime();
     }
 
