b2fab5d810b2026517695304c93add5c7753d26a
Matheus Martins <matheusmartinsdeveloper14@gmail.com>
2025-08-21 11:22:49 -0300

    Aula 67

---
+++ src/javacore/Gassociacao/test/JogadorTest03.java
@@ -0,0 +1,25 @@
+package javacore.Gassociacao.test;
+
+import javacore.Gassociacao.dominio.Jogador;
+import javacore.Gassociacao.dominio.Time;
+
+public class JogadorTest03 {
+    public static void main(String[] args) {
+        Jogador jogador1 = new Jogador("Cafu");
+        Jogador jogador2 = new Jogador("Pelé");
+        Time time = new Time("Brasil");
+        Jogador[] jogadores = {jogador1, jogador2};
+
+        jogador1.setTime(time);
+        jogador2.setTime(time);
+        time.setJogadores(jogadores);
+
+        System.out.println("------- Jogador --------");
+        System.out.println("------- Time --------");
+
+        jogador1.imprime();
+        jogador2.imprime();
+        time.imprime();
+
+    }
+}
