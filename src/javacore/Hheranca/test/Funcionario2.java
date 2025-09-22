0158ff3f93027ddb75b65045a3ed03bfc27fc8ef
Matheus Martins <matheusmartinsdeveloper14@gmail.com>
2025-09-22 11:11:46 -0300

    Aula 74

--- src/javacore/Hheranca/test/Funcionario2.java
+++
@@ -1,9 +0,0 @@
-package javacore.Hheranca.test;
-
-import javacore.Hheranca.dominio.Pessoa;
-
-public class Funcionario2 extends Pessoa {
-    public void imprime(){
-        this.nome = "Sasuke";
-    }
-}
