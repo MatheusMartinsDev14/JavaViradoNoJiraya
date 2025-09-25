492a8c9a9604f78139a136f66e1413fb8f88c740
Matheus Martins <matheusmartinsdeveloper14@gmail.com>
2025-09-25 09:00:01 -0300

    Aula 81

--- src/javacore/Kenum/dominio/TipoCliente.java
+++ src/javacore/Kenum/dominio/TipoCliente.java
@@ -1,8 +1,22 @@
 package javacore.Kenum.dominio;
 
 public enum TipoCliente {
+    PESSOA_FISICA(1, "Pessoa Física"),
+    PESSOA_JURIDICA(2, "Pessoa Jurídica");
 
-    PESSOA_FISICA,
-    PESSOA_JURIDICA
+    private int valor;
+    private String nomeRelatorio;
+
+    TipoCliente(int valor, String nomeRelatorio){
+        this.valor = valor;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public String getNomeRelatorio() {
+        return nomeRelatorio;
+    }
 }
 
--- src/javacore/Kenum/test/ClienteTest01.java
+++ src/javacore/Kenum/test/ClienteTest01.java
@@ -1,24 +1,19 @@
 package javacore.Kenum.test;
 
 import javacore.Kenum.dominio.Cliente;
+import javacore.Kenum.dominio.Cliente.TipoPagamento;
 import javacore.Kenum.dominio.TipoCliente;
 
-import java.sql.SQLOutput;
+
 
 public class ClienteTest01 {
 
     public static void main(String[] args) {
 
-        Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA);
-        Cliente cliente2 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA);
-        Cliente cliente3 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA);
-        Cliente cliente4 = new Cliente("Tsubasa", TipoCliente.PESSOA_JURIDICA);
-        Cliente cliente5 = new Cliente("Tsubasa", TipoCliente.PESSOA_JURIDICA);
+        Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA, TipoPagamento.DEBITO);
+        Cliente cliente2 = new Cliente("Akira", TipoCliente.PESSOA_JURIDICA, TipoPagamento.CREDITO);
 
         System.out.println(cliente1);
         System.out.println(cliente2);
-        System.out.println(cliente3);
-        System.out.println(cliente4);
-        System.out.println(cliente5);
     }
 }
