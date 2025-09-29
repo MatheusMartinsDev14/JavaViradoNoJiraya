154790aa4bae0a3473357adfd3ac3015b1884a3d
Matheus Martins <matheusmartinsdeveloper14@gmail.com>
2025-09-29 13:58:47 -0300

    Aula 88

--- src/javacore/Minterfaces/dominio/DataLoader.java
+++ src/javacore/Minterfaces/dominio/DataLoader.java
@@ -2,4 +2,7 @@ package javacore.Minterfaces.dominio;
 
 public interface DataLoader {
     public abstract void load();
+    default void checkPermission(){
+        System.out.println("Fazendo Checagem de permissões");
+    }
 }
